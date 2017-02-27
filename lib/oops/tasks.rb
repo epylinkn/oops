2bb891bcc7e5c2a477f29ae15fc0c45dfbaeee93
Zachary Friedman <zac@dia.co>
2017-02-27 13:05:03 -0500

    Include public/sitemap.xml.gz in included files for build

--- lib/oops/tasks.rb
+++ lib/oops/tasks.rb
@@ -10,7 +10,7 @@ module Oops
       {
         prerequisites: ['assets:clean', 'assets:precompile'],
         additional_paths: [],
-        includes: ['public/assets'],
+        includes: ['public/assets', 'public/sitemap.xml.gz'],
         excludes: ['.gitignore'],
         format: 'zip'
       }
